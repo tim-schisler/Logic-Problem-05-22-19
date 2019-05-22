131d6c4c6ce69f2f5b398f0f157818640ca34df5
bert <bert@DESKTOP-MDVT4UA.attlocal.net>
2019-05-21 21:40:14 -0500

    Creates the layout of the GUI. Adds an opening comment describing the problem this program solves.

--- src/logicproblem0522/LogicProblem0522.java
+++ src/logicproblem0522/LogicProblem0522.java
@@ -1,4 +1,22 @@
 /*
+    Tim Schisler
+    22 May 2019
+    Solution to logic problem for Charter application
+
+    "A retailer offers a rewards program to its customers,
+    awarding points based on each recorded purchase. 
+
+    A customer receives 2 points for every dollar spent over $100
+    in each transaction, plus 1 point for every dollar spent
+    over $50 in each transaction 
+    (e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
+
+    Given a record of every transaction during a three month period,
+    calculate the reward points earned for each customer
+    per month and total.
+
+    · Make up a data set to best demonstrate your solution
+    · Check solution into GitHub"
 
 */
 package logicproblem0522;
@@ -7,11 +25,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.event.*;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 
@@ -47,20 +65,44 @@ public class LogicProblem0522 extends Application {
     /**
      start method: sets up the GUI for this JavaFX application.
      */
+    private TextField currentMonth = new TextField();
+    private TextField lastMonth = new TextField();
+    private TextField beforeMonth = new TextField();
+    private TextField threeMonthTotal = new TextField();
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent e) -> {
-            System.out.println("Hello World!");
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //Create the basic layout of the GUI.
+        Text instructions = new Text();
+        ComboBox cb = new ComboBox();
+        GridPane summaryPane = new GridPane();
         
+        //Add text to the instructions field to direct the user.
+        String instr = "To view a summary of rewards for a customer,\n"
+                + "select a customer ID from the dropdown list.";
+        instructions.setText(instr);
+        
+        //Query the db for a list of customer IDs; add them to cb.
+        
+        
+        //Add the labels and fields to the summary pane.
+        summaryPane.add(new Label("Rewards This Month:"), 0, 0);
+        summaryPane.add(currentMonth, 1, 0);
+        summaryPane.add(new Label("Rewards Last Month:"), 0, 1);
+        summaryPane.add(lastMonth, 1, 1);
+        summaryPane.add(new Label("Rewards the Month Before:"), 0, 2);
+        summaryPane.add(beforeMonth, 1, 2);
+        summaryPane.add(new Label("Three Month Total:"), 0, 3);
+        summaryPane.add(threeMonthTotal, 1, 3);
+        
+        //Prepare the scene.
+        VBox root = new VBox();
+        root.getChildren().addAll(instructions, cb, summaryPane);
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        //Set up and show the application stage.
+        primaryStage.setTitle("Rewards");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
